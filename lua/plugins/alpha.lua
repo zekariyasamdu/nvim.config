46cde7605a4255fa81052949869cf056b90831d2
zach <zekariyasamdu24@gmail.com>
2026-02-27 16:55:42 -0600

    fix

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -7,25 +7,35 @@ return {
 
 		-- Set header
 		dashboard.section.header.val = {
-			"                                                     ",
-			"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-			"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-			"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-			"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-			"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-			"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-			"                                                     ",
+			[[                                                ]],
+			[[                                          _.oo. ]],
+			[[                  _.u[[/;:,.         .odMMMMMM' ]],
+			[[               .o888UU[[[/;:-.  .o@P^    MMM^   ]],
+			[[              oN88888UU[[[/;::-.        dP^     ]],
+			[[             dNMMNN888UU[[[/;:--.   .o@P^       ]],
+			[[            ,MMMMMMN888UU[[/;::-. o@^           ]],
+			[[            NNMMMNN888UU[[[/~.o@P^              ]],
+			[[            888888888UU[[[/o@^-..               ]],
+			[[           oI8888UU[[[/o@P^:--..                ]],
+			[[        .@^  YUU[[[/o@^;::---..                 ]],
+			[[      oMP     ^/o@P^;:::---..                   ]],
+			[[   .dMMM    .o@^ ^;::---...                     ]],
+			[[  dMMMMMMM@^`       `^^^^                       ]],
+			[[ YMMMUP^                                        ]],
+			[[  ^^                                            ]],
+			[[                                                ]],
 		}
 
 		-- Set menu
 		dashboard.section.buttons.val = {
-			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Oil<CR>"),
+			dashboard.button("SPC sr", "󰁯 > Restore Session For Current Directory", "<cmd>AutoSession restore<CR>"),
+			dashboard.button("SPC ee", " > Toggle file explorer", "<cmd>Oil<CR>"),
 			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-			dashboard.button("SPC sr", "󰁯  > Restore Session For Current Directory", "<cmd>AutoSession restore<CR>"),
+			dashboard.button("SPC fs", " > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
+
+		dashboard.section.footer.val = "Beautiful, naked, big-titty womеn just don't fall out the sky, you know?!"
 
 		-- Send config to alpha
 		alpha.setup(dashboard.opts)
--- lua/zach/keymaps.lua
+++ lua/zach/keymaps.lua
@@ -1,5 +1,12 @@
 -- oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 -- cusom
-vim.keymap.set("n", "<leader>tn", ":tabnew<CR>")
+
+-- Tabs
+vim.keymap.set("n", "<leader>ta", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>tl", ":tabs<CR>")
+for i = 1, 9 do
+	vim.keymap.set("n", "<leader>t" .. i, function()
+		vim.cmd("tabn " .. i)
+	end)
+end
