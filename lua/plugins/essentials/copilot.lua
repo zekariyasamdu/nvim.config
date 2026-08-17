c414645d3cfcaffdf8a35e326a89053d19a2c163
zekariyasamdu <zekariyasamdu24@gmail.com>
2026-08-17 13:57:33 +0300

    fix

--- lua/plugins/essentials/copilot.lua
+++ lua/plugins/essentials/copilot.lua
@@ -2,6 +2,6 @@ return {
 	"github/copilot.vim",
 	config = function()
 		vim.g.copilot_no_tab_map = true
-		vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+		vim.api.nvim_set_keymap("i", "<C-A>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 	end,
 }
