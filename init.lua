f8b623bcad9a6ef35b65529a9a2261e6f4926b16
zekariyasamdu <zekariyasamdu24@gmail.com>
2026-09-01 21:09:52 +0300

    cleanup

--- init.lua
+++ init.lua
@@ -1,10 +1,1 @@
--- Customs
- require("zach.globals")
- require("zach.options")
- require("zach.keymaps")
- require("zach.usercmd")
- require("zach.autocmd")
--- Package manager
- require("config.lazy")
--- lsp collections
- require("lsp.lsp")
+
